c0bb35dd8052b23549ad1ee127040ef76e3014ab
ZhengKeli <zhengkeli2009@126.com>
2018-02-16 15:00:19 +0700

    a reformat

--- src/zkl/scienceFX/wave/physics/aparapi/AparapiWaveWorld.kt
+++ src/zkl/scienceFX/wave/physics/aparapi/AparapiWaveWorld.kt
@@ -3,35 +3,45 @@ package zkl.scienceFX.wave.physics.aparapi
 import zkl.scienceFX.wave.physics.abstracts.*
 import java.util.*
 
-class AparapiWaveWorld(draft: WaveWorldDraft):WaveWorld {
+class AparapiWaveWorld(draft: WaveWorldDraft) : WaveWorld {
 	
-	override val units: List<WaveUnit> = object :AbstractList<WaveUnit>(){
+	override val units: List<WaveUnit> = object : AbstractList<WaveUnit>() {
 		override val size: Int get() = kernel.unitsCount
 		override fun get(index: Int): WaveUnit = kernel.run {
 			object : WaveUnit {
 				override val id: Int get() = index
 				override var offset: Float
-					get() = if (computeCount%2 == 0) unitsOffset_s0[id] else unitsOffset_s1[id]
-					set(value) { if (computeCount%2 == 0) unitsOffset_s0[id] = value else unitsOffset_s1[id] = value }
+					get() = if (computeCount % 2 == 0) unitsOffset_s0[id] else unitsOffset_s1[id]
+					set(value) {
+						if (computeCount % 2 == 0) unitsOffset_s0[id] = value else unitsOffset_s1[id] = value
+					}
 				override var velocity: Float
 					get() = unitsVelocity[id]
-					set(value) { unitsVelocity[id] = value }
+					set(value) {
+						unitsVelocity[id] = value
+					}
 				override var mass: Float
 					get() = unitsMass[id]
-					set(value) { unitsMass[id] = value }
+					set(value) {
+						unitsMass[id] = value
+					}
 				override var damping: Float
 					get() = unitsDamping[id]
-					set(value) { unitsDamping[id] = value }
+					set(value) {
+						unitsDamping[id] = value
+					}
 				override var extra: Any?
 					get() = unitsExtra[id]
-					set(value) { unitsExtra[id] = value }
+					set(value) {
+						unitsExtra[id] = value
+					}
 			}
 		}
 	}
-	override val links: List<WaveLink> = object :AbstractList<WaveLink>(){
+	override val links: List<WaveLink> = object : AbstractList<WaveLink>() {
 		override val size: Int get() = kernel.linksCount
 		override fun get(index: Int): WaveLink = kernel.run {
-			object: WaveLink {
+			object : WaveLink {
 				override val unitId1: Int
 					get() = kernel.run { impactsFromUnitId[linksImpactId2[index]] }
 				override val unitId2: Int
@@ -44,13 +54,16 @@ class AparapiWaveWorld(draft: WaveWorldDraft):WaveWorld {
 					}
 				override var extra: Any?
 					get() = linksExtra[index]
-					set(value) { linksExtra[index] = value }
+					set(value) {
+						linksExtra[index] = value
+					}
 			}
 		}
 	}
-	override var extra: Any? =  draft.extra
+	override var extra: Any? = draft.extra
 	
-	override val invokers:MutableList<WaveInvoker> = LinkedList()
+	override val invokers: MutableList<WaveInvoker> = LinkedList()
+	
 	
 	val kernel: AparapiKernel = kotlin.run {
 		println("launching OpenCL ...")
@@ -61,14 +74,18 @@ class AparapiWaveWorld(draft: WaveWorldDraft):WaveWorld {
 		
 		return@run kernel
 	}
-	override val time: Float get() {
-		kernel.run{
-			return if (!isExecuting) time else time+currentPass*timeUnit
+	
+	override val time: Float
+		get() {
+			kernel.run {
+				return if (!isExecuting) time else time + currentPass * timeUnit
+			}
 		}
-	}
-	@Synchronized override fun process(timeUnit: Float, count: Int) {
+	
+	@Synchronized
+	override fun process(timeUnit: Float, count: Int) {
 		kernel.process(count, timeUnit, invokers)
-		synchronized(invokers) { invokers.removeIf { time>it.startTime + it.span } }
+		synchronized(invokers) { invokers.removeIf { time > it.startTime + it.span } }
 	}
 	
 }
